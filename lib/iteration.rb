053183a4d5a496e4bd5ca91f5d134f8a207237a3
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-26 20:39:23 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -12,6 +12,7 @@ def join_ingredients(src)
   while row_index < src.length do
     new_array.push("I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza")
     row_index += 1
+  end
   return new_array
 
 end
