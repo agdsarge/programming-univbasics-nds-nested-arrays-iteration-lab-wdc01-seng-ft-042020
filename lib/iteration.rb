97dd90370b29bc59f4c7ced471b98a44ee7396cb
Andrew Sargeant <agdsargeant@gmail.com>
2020-03-26 20:45:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -42,8 +42,8 @@ def total_even_pairs(src)
   total = 0
   row_index = 0
   while row_index < src.length do
-    total = src[row_index][0] + src[row_index][1]
-    if total % 2 == 0
-      total 
+    sum = src[row_index][0] + src[row_index][1]
+    if sum % 2 == 0 
+      total += sum
   end
 end
